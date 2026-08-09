44f83d77ce42caedbf448c473e94acbf032dba73
Amit4700 <amitsingh47002@gmail.com>
2026-08-09 20:48:36 +0530

    add_code

--- enveroment/preprod/variable.tf
+++ enveroment/preprod/variable.tf
@@ -1,6 +1,22 @@
 variable "rgs" {}
-variable "vnets" {}
-variable "subnets" {}
+variable "vnets" {
+  type = map(object({
+    name          = string
+    location      = string
+    rg_name       = string
+    address_space = list(string)
+  }))
+  description = "Map of virtual networks to create"
+}
+variable "subnets" {
+  type = map(object({
+    subnet_name      = string
+    rg_name          = string
+    vnet_name        = string
+    address_prefixes = list(string)
+  }))
+  description = "Map of subnets to create"
+}
 variable "pips" {}
 variable "vms" {}
 variable "bastion_app" {}
